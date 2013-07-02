44b91216f923d16ac166e32f347376ead91f9335
faheem <faheem@faheempatel.com>
2013-07-02 05:04:34 +0100

    Refactor

--- referencer.rb
+++ referencer.rb
@@ -9,9 +9,48 @@ end
 post '/result' do
     input = params[:raw_list].split("\n")
     
-    # removes any empty elements in array
-    @isbns = input.reject { |x| x.strip.length == 0 }
+    # Removes any empty elements in array
+    isbns = input.reject { |x| x.strip.length == 0 }
+
+    # Get references
+    @references = ""
+    isbns.each do |isbn|
+        @references << get_book_reference(isbn.strip) + "\n"
+    end
     
-    @details = Openlibrary::Data
     haml :result
+end
+
+def get_book_reference(isbn)
+    details = Openlibrary::Data
+    
+    book       = details.find_by_isbn(isbn)
+    title      = book.title
+    date       = book.publish_date
+    publishers = book.publishers
+    authors    = book.authors
+
+    # Build up reference
+    reference = ""
+     
+    # Author(s)
+    authors.each do |author|
+        reference << "#{ author["name"] }, "
+    end
+
+    # Date
+    reference << "#{ date }. "
+      
+    # Title
+    reference << "#{ title }. "
+
+    # Publisher(s)
+    count = publishers.length
+    publishers.each do |publisher|
+        punctuation = count > 1 ? "," : "."
+        reference << "%s%s" % [publisher["name"], punctuation]
+        count -= 1
+    end
+
+    return reference
 end
